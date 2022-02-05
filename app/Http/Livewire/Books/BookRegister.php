0c1a3a40e0ec8aef4eec5af4ce5c1937fd265f35
Gerardo A Belot <gbelot2003@hotmail.com>
2022-02-05 21:37:49 +0000

    🏃 ON: maketando libro mayor

--- app/Http/Livewire/Books/BookRegister.php
+++ app/Http/Livewire/Books/BookRegister.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Books;
 
+use App\Models\Account;
 use Livewire\Component;
 
 class BookRegister extends Component
 {
+
+
+
     public function render()
     {
-        return view('livewire.books.book-register');
+        $cuentas = Account::all();
+        return view('livewire.books.book-register', compact('cuentas'));
     }
 }
--- resources/views/livewire/books/book-register.blade.php
+++ resources/views/livewire/books/book-register.blade.php
@@ -1,3 +1,53 @@
-<div>
-    <h2>Books</h2>
+<div class="row">
+    <div class="col-md-2">
+        <label for="account">Desde</label>
+        <select name="cuenta" id="" class="form-control">
+            <option value="">Cuenta</option>
+            @foreach ($cuentas as $row)
+                <option value="{{ $row->id }}">{{ $row->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="col-md-3">
+        <label for="account">Debe</label>
+        <input type="number" name="valor" class="form-control" />
+    </div>
+
+    <div class="col-md-3">
+        <label for="account">Haber</label>
+        <input type="number" name="valor" class="form-control" />
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-md-2">
+        <label for="account">Desde</label>
+        <select name="cuenta" id="" class="form-control">
+            <option value="">Cuenta</option>
+            @foreach ($cuentas as $row)
+                <option value="{{ $row->id }}">{{ $row->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="col-md-3">
+        <label for="account">Debe</label>
+        <input type="number" name="valor" class="form-control" />
+    </div>
+
+    <div class="col-md-3">
+        <label for="account">Haber</label>
+        <input type="number" name="valor" class="form-control" />
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="col-md-8">
+        <label for="description">Description</label>
+        <textarea name="description" name="description" class="form-control" cols="30" rows="4"></textarea>
+    </div>
 </div>
